910e7927aa9c9be29b693978791c9f6b452abab0
Roberto-Luis-Rodriguez <robertorodriguezjr86@gmail.com>
2017-10-28 09:01:29 -0400

    First Commit

---
+++ code.rb
@@ -0,0 +1,26 @@
+require 'pg'
+require 'csv'
+
+def db_connection
+  begin
+    connection = PG.connect(dbname: "ingredients")
+    yield(connection)
+  ensure
+    connection.close
+  end
+end
+
+CSV.foreach('ingredients.csv', headers: true) do |row|
+  db_connection do |conn|
+    insert_ingredients = "INSERT INTO ingredients (step, name) VALUES ($1, $2)"
+    values = [row['step'], row['name']]
+    conn.exec_params(insert_ingredients, values)
+  end
+end
+
+select_ingredients = "SELECT * FROM ingredients"
+ingredients = db_connection  do |conn|
+  conn.exec(select_ingredients)
+end
+
+puts ingredients.to_a  
